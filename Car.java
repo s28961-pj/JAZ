335444c587b865aa04373fe0339be69f3566f2ae
Roland Liedtke <115408545+s28961-pj@users.noreply.github.com>
2024-04-06 19:13:27 +0200

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,23 @@
+package pl.pjatk.rollie.model;
+
+public class Car {
+    public String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public Car() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
